58f5f83f8230577835b2543539b8c2fa7fa490b4
ripboy <rishikesh.330@gmail.com>
2016-12-06 17:51:09 -0500

    submission 1

---
+++ customer/classes/User.php
@@ -0,0 +1,19 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","ecommerce");
+
+class User {
+	function getIp() {
+    $ip = $_SERVER['REMOTE_ADDR'];
+ 
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+ 
+    return $ip;
+	}
+}
+
+?>
